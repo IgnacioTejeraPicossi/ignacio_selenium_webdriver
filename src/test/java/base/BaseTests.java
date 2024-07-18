988e27808522cc229aa20ddb3c80f519b4d86d03
IgnacioTejeraPicossi <ignacio.tejera@outlook.com>
2024-07-18 10:54:01 +0200

    Include LoginPage and SecureAreaPage

--- src/test/java/base/BaseTests.java
+++ src/test/java/base/BaseTests.java
@@ -7,7 +7,7 @@ public class BaseTests {
     public void setUp(){
         System.setProperty("web-driver.chrome.driver", "resources/chromedriver.exe");
         driver = new ChromeDriver();
-        driver.get("https://the-internet.herokuapp.com/");
+        driver.get("https://item.no/");
         System.out.println(driver.getTitle());
     }
     public static void main(String []args){
