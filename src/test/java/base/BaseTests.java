2d420235ff8042b23ba45d8854453e8e216bffc0
IgnacioTejeraPicossi <ignacio.tejera@outlook.com>
2024-07-19 13:35:28 +0200

    Fix LoginPage and SecureAreaPage

--- src/test/java/base/BaseTests.java
+++ src/test/java/base/BaseTests.java
@@ -1,19 +1,27 @@
 package base;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import pages.HomePage;
 
 public class BaseTests {
+
     private WebDriver driver;
+    protected HomePage homePage;
+
+    @BeforeClass
     public void setUp(){
-        System.setProperty("web-driver.chrome.driver", "resources/chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", "resources/chromedriver");
         driver = new ChromeDriver();
-        driver.get("https://item.no/");
-        System.out.println(driver.getTitle());
-    }
-    public static void main(String []args){
-        BaseTests test = new BaseTests();
-        test.setUp();
+        driver.get("https://the-internet.herokuapp.com/");
+
+        homePage = new HomePage(driver);
     }
 
-
+    @AfterClass
+    public void tearDown(){
+        driver.quit();
+    }
 }
